273005c5a0b6cf5ef3e9a88a104791b3c2f6377f
ThejasBK <thejaskiran99@gmail.com>
2020-09-19 18:00:35 +0530

    Need to send data

--- MyProfiles/Index/views.py
+++ MyProfiles/Index/views.py
@@ -11,9 +11,7 @@ def register(request):
         social_media_site = request.POST.get('inputSocialMedia')
         user_name = request.POST.get('inputUserName')
         passowrd = request.POST.get('inputPassowrd')
-        print(social_media_site, user_name, passowrd)
-        enc_password = pbkdf2_sha256.encrypt(passowrd, rounds = 12000, salt_size = 32)
-        
+        #enc_password = pbkdf2_sha256.encrypt(passowrd, rounds = 12000, salt_size = 32)
         Profile.objects.create(
             social_media = social_media_site,
             user_name = user_name,
