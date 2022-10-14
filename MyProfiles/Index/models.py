8b57298e6708af957f53941cccd51115797c461a
Thejas B K <50453786+ThejasBK@users.noreply.github.com>
2022-10-13 19:06:25 -0600

    Revert "Site to add new user information has been created. Need to build the …"

--- MyProfiles/Index/models.py
+++ MyProfiles/Index/models.py
@@ -2,15 +2,21 @@ from django.db import models
 from MyProfiles import settings
 from django.contrib.auth.hashers import make_password
 
-class User(models.Model):
-    user_name = models.CharField(max_length = 264, null = False)
-    password = models.CharField(max_length = 32, null = False)
-
+# Create your models here.
 class Profile(models.Model):
     '''Profile data table handles the username and passwords of other websites for each respective user'''
+    # user_id = models.ForeignKey(models.LogIn, on_delete = models.CASCADE)
     social_media = models.CharField(max_length = 264, null = True)
     user_name = models.CharField(max_length = 264, null = True)
     password = models.CharField(max_length = 1000, null = True)
 
     def __str__(self):
         return self.name
+
+# class LogIn(models.Model):
+#     '''LogIn data table consists of user name and password to login to the MyProfiles portal'''
+#     user_name = models.CharField(max_length = 264, unique = True)
+#     password = models.CharField(max_length = 1000, null = True) 
+
+#     def __str__(self):
+#         return self.name
